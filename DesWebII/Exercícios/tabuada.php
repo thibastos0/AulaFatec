4253a541aad0aabdb1d6a383e2a37f7e62c7b894
thibastos0 <thibastos@live.com>
2025-03-19 15:18:37 -0300

    aula003 e praticando

---
+++ DesWebII/Exercícios/tabuada.php
@@ -0,0 +1,26 @@
+<?php
+
+function tabuada($n){
+    for($i = 1; $i <= 10; $i++){
+        echo "$n x $i = " . $n * $i . ". <br>";
+    }
+}
+
+$num = $_GET['num'];
+echo "Imprimindo a tabuada do número $num: <br>";
+echo "<hr>";
+echo tabuada($num);
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabuada</title>
+</head>
+<body>
+    <input type="number" name="num" id="num" placeholder="Digite um número">
+    <button onclick="window.location.href='tabuada.php?num=' + document.getElementById('num').value">Gerar tabuada</button>
+</body>
+</html>
