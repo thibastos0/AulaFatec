c9a828d179054d7ee0be6a902f52ddb46a8e5ab3
thibastos0 <thibastos@live.com>
2025-09-12 09:16:16 -0300

    aula 12SET2025

--- TecProgramacaoII/ExemploSingleton/src/exemplos/Main.java
+++
@@ -1,8 +0,0 @@
-package exemplos;
-
-public class Main {
-    public static void main(String[] args) {
-        Exemplo teste = new Exemplo("Testando atributo publico");
-        teste.verExemplo();
-    }
-}
