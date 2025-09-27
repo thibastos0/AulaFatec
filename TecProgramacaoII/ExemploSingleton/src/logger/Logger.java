c8297f6358a3d92d96fc09ed7c4c29de56a370aa
thibastos0 <thibastos@live.com>
2025-09-27 10:03:29 -0300

    Atividade Log: 3. siulando singleton em ambiente multithread

--- TecProgramacaoII/ExemploSingleton/src/logger/Logger.java
+++ TecProgramacaoII/ExemploSingleton/src/logger/Logger.java
@@ -5,14 +5,22 @@ import java.time.LocalTime;
 public class Logger {
 
     private static Logger instance;
+    public int numIntance;
 
-    private Logger(){
-        System.out.println("Novo Logger criado");
+    private Logger(int numIntance){
+        // Construtor simulando a inicialização lenta
+        try {
+            Thread.sleep(1000); //aguarda 1 segundo
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.numIntance = numIntance;
+        System.out.println("Novo Logger criado de número " + this.numIntance);
     }
 
-    public static Logger getInstance(){
+    public static Logger getInstance(int numIntance){
         if (instance == null) {
-            instance = new Logger();
+            instance = new Logger(numIntance);
         }
         return instance;
     }
