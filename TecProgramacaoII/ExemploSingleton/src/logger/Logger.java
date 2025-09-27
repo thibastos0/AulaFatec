14161b315b8f94e29fb908498147fa770af25cf5
thibastos0 <thibastos@live.com>
2025-09-27 09:33:47 -0300

    Atividade Logger:  2. após aplicado Singleton

--- TecProgramacaoII/ExemploSingleton/src/logger/Logger.java
+++ TecProgramacaoII/ExemploSingleton/src/logger/Logger.java
@@ -1,14 +1,31 @@
 package logger;
 
+import java.time.LocalTime;
+
 public class Logger {
 
-    public Logger(){
+    private static Logger instance;
+
+    private Logger(){
         System.out.println("Novo Logger criado");
     }
 
+    public static Logger getInstance(){
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
     public void log(String message) {
-        System.out.println("LOG:" + message);
-        System.out.println(this.toString());
+        LocalTime time = LocalTime.now();
+        System.out.println(time + " - LOG:" + message);
+        System.out.println("Instância: " + this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Logger@" + Integer.toHexString(System.identityHashCode(this));
     }
 
 }
--- TecProgramacaoII/ExemploSingleton/src/logger/MainLogger.java
+++ TecProgramacaoII/ExemploSingleton/src/logger/MainLogger.java
@@ -3,10 +3,12 @@ package logger;
 public class MainLogger {
 
     public static void main(String[] args) {
-        Logger logger1 = new Logger();
+        //Logger logger1 = new Logger();
+        Logger logger1 = Logger.getInstance();
         logger1.log("Primeira mensagem");
 
-        Logger logger2 = new Logger();
+        //Logger logger2 = new Logger();
+        Logger logger2 = Logger.getInstance();
         logger2.log("Segunda mensagem");
 
     }
